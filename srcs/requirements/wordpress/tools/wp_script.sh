6eea0d037b3adf36818ac019e66d99a275027b84
mmakarov <mmakarov@student.42lausanne.ch>
2024-03-14 17:53:44 +0100

    14.03

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -7,14 +7,15 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 # Download wordpress
-wp core download --allow-root --path=/var/www/html/ 
+wp core download --allow-root --path=/usr/share/nginx/html
 
 # Change ownership to give rights to the root
-chown -R www-data:www-data /var/www/html/
+chown -R www-data:www-data /usr/share/nginx/html
 
 # Create the configuration file wp-config.php of wordpress
-wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} 
+# wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} 
 
 # Install wordpress
-wp core install --allow-root --url=localhost --title="My_website" --admin_user=${DB_USER} --admin_password=${DB_ROOT_PASSWORD} --admin_email=mmakarov@student.42lausanne.ch
+# wp core install --allow-root --url=localhost --title="My_website" --admin_user=${DB_USER} --admin_password=${DB_ROOT_PASSWORD} --admin_email=mmakarov@student.42lausanne.ch
 
+/usr/sbin/php-fpm7.4 -F
