8b2b9402526752e731d68d5e76f56fb800d20fa7
mmakarov <mmakarov@student.42lausanne.ch>
2024-03-15 17:47:58 +0100

    15.03 ok without vm?

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -9,13 +9,16 @@ mv wp-cli.phar /usr/local/bin/wp
 # Download wordpress
 wp core download --allow-root --path=/usr/share/nginx/html
 
+# Give permissions 
+chmod 755 /usr/share/nginx/html/*
+
 # Change ownership to give rights to the root
 chown -R www-data:www-data /usr/share/nginx/html
 
 # Create the configuration file wp-config.php of wordpress
-# wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} 
+wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} 
 
 # Install wordpress
-# wp core install --allow-root --url=localhost --title="My_website" --admin_user=${DB_USER} --admin_password=${DB_ROOT_PASSWORD} --admin_email=mmakarov@student.42lausanne.ch
+wp core install --allow-root --url=localhost --title="My_website" --admin_user=${DB_USER} --admin_password=${DB_ROOT_PASSWORD} --admin_email=mmakarov@student.42lausanne.ch
 
 /usr/sbin/php-fpm7.4 -F
