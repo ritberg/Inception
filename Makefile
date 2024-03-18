03ff08d47764c1c41e29941ae8616d0282a5f8f8
mmakarov <mmakarov@student.42lausanne.ch>
2024-03-18 12:12:52 +0100

    18.03 mariadb error

--- Makefile
+++ Makefile
@@ -1,1 +1,40 @@
-dkfjsflkd
+SRC_DIR := srcs
+ENV_FILE := $(SRC_DIR)/.env
+DOCKER_COMPOSE := docker-compose -f $(SRC_DIR)/docker-compose.yml --env-file=$(ENV_FILE)
+
+# make without args
+.DEFAULT_GOAL := help
+
+help:
+	@echo "Usage: make <target>"
+	@echo ""
+	@echo "Available targets:"
+	@echo "  build          Build Docker containers"
+	@echo "  up          Start Docker containers"
+	@echo "  down           Stop Docker containers"
+	@echo "  restart        Restart Docker containers"
+	@echo "  logs           View logs of Docker containers"
+	@echo "  clean          Remove Docker containers and volumes"
+	@echo "  help           Display this help message"
+
+
+build:
+	$(DOCKER_COMPOSE) build
+
+up:
+	$(DOCKER_COMPOSE) up -d
+
+down:
+	$(DOCKER_COMPOSE) down
+
+restart: stop start
+
+logs:
+	$(DOCKER_COMPOSE) logs -f
+
+clean:
+	$(DOCKER_COMPOSE) down -v
+
+clean_cache:
+	docker system prune -af
+
--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if WordPress files exist
+if [ -d "/usr/share/nginx/html" ]; then
+    echo "WordPress files already exist. Deleting existing files..."
+    rm -rf /usr/share/nginx/html/*
+fi
+
 # Download WP-CLI to be able to use wp core download etc.
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
@@ -15,10 +21,13 @@ chmod 755 /usr/share/nginx/html/*
 # Change ownership to give rights to the root
 chown -R www-data:www-data /usr/share/nginx/html
 
+# Change directory to WordPress installation directory
+cd /usr/share/nginx/html
+
 # Create the configuration file wp-config.php of wordpress
-wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} 
+wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST}
 
 # Install wordpress
-wp core install --allow-root --url=localhost --title="My_website" --admin_user=${DB_USER} --admin_password=${DB_ROOT_PASSWORD} --admin_email=mmakarov@student.42lausanne.ch
+wp core install --allow-root --url=127.0.0.1 --title="My_website" --admin_user=${DB_USER} --admin_password=${DB_ROOT_PASSWORD} --admin_email=${DB_ROOT_EMAIL}
 
 /usr/sbin/php-fpm7.4 -F
