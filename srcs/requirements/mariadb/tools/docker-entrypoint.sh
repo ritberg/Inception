5246b6e710fcf08d49a20328fdbcc8afd92a92e3
mmakarov <mmakarov@student.42lausanne.ch>
2024-03-28 16:12:30 +0100

    28.03

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT SIGHUP
-
-some_app &
-wait
---
+++ srcs/requirements/mariadb/tools/kill_pid1.sh
@@ -0,0 +1,12 @@
+asyncRun() {
+    "$@" &
+    pid="$!"
+    trap "echo 'Stopping PID $pid'; kill -SIGTERM $pid" SIGINT SIGTERM
+
+    while kill -0 $pid > /dev/null 2>&1; do
+        wait
+    done
+}
+asyncRun /db_script.sh $@
+
+#source: https://github.com/docker-library/mysql/issues/47, jeremyVignelles
